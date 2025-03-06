ab6fae1789d4c5ce6e66deb526d8ff7e401eb94c
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-06 16:39:41 +0600

    navbar created.......

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        <h2 className="text-6xl text-red-600">Hello corporate</h2>
-       <p className="text-6xl text-green-600 text-start">all in one</p>
+       <p className="text-6xl text-green-600 text-start">all in oneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
       </main>
       
     </div>
