2d920cdb8ac0d575e12d0ddaee1fbb4ed9760ffb
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-12 10:09:15 +0600

    our service 2 created for home 2

--- src/app/home2/page.tsx
+++ src/app/home2/page.tsx
@@ -1,11 +1,13 @@
 import Banner2 from "@/components/view/Banner2";
 import Navbar2 from "@/components/view/Navbar2";
+import OurService2 from "@/components/view/OurService2";
 
 export default function Home2() {
     return (
-      <div className="min-h-screen flex justify-center items-center">
+      <div className="min-h-screen flex flex-col justify-center items-center">
         <Navbar2 />
         <Banner2 />
+        <OurService2 />
       </div>
     );
   }
