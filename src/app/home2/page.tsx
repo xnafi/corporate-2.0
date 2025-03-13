7d857d8c3b0b119752f3b5715ccf9d7edc03a354
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-13 10:04:07 +0600

    Latest news  created in home 2

--- src/app/home2/page.tsx
+++ src/app/home2/page.tsx
@@ -2,6 +2,7 @@ import AboutUs2 from "@/components/view/AboutUs2";
 import Banner2 from "@/components/view/Banner2";
 import Feedback2 from "@/components/view/Feedback2";
 import Inquiries from "@/components/view/Inquiries";
+import LatestNews from "@/components/view/LatestNews";
 import Member2 from "@/components/view/Member2";
 import Navbar2 from "@/components/view/Navbar2";
 import OurService2 from "@/components/view/OurService2";
@@ -17,6 +18,7 @@ export default function Home2() {
         <Member2 />
         <OurWork />
         <Feedback2 />
+        <LatestNews />
         <Inquiries />
       </div>
     );
