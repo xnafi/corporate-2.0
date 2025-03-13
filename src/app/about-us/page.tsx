38d2f1baafcae8201d0c348046f240751586c7ff
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-13 11:02:32 +0600

    About us page created with banner

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+
+const AboutUs = () => {
+  return (
+    <div>
+      {/* Banner Section */}
+      <div className="relative w-full h-[400px] flex items-center justify-center bg-black">
+        <Image
+          src="https://i.postimg.cc/bwTh0nJQ/look-studio.jpg"
+          alt="About Us Banner"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-50"
+        />
+        <div className="absolute text-center text-white">
+          <h1 className="text-6xl font-extrabold">About Us</h1>
+          <p className="mt-2">Home / Pages / About Us</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
