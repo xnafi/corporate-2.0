60a8d932f010f88d16bcfd26102ff14aae4fb499
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-16 11:38:50 +0600

    Mobile responsiveness of our services created

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -97,8 +97,9 @@ const Navbar = () => {
             <IoIosArrowDown />
             {isServicesOpen && (
               <ul className="absolute left-0 top-10 bg-white shadow-lg py-2 w-40 md:w-32 border border-gray-200">
-                <li className="px-4 py-2 hover:bg-gray-100">Service 1</li>
-                <li className="px-4 py-2 hover:bg-gray-100">Service 2</li>
+               <li className="px-4 py-2 hover:bg-gray-100">
+                  <Link href="/our-services">Our Services</Link>
+                </li>
               </ul>
             )}
           </li>
