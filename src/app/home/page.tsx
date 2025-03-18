e03b0e7ed60b3857b12efc534d9c9f523e941b30
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-18 08:46:44 +0600

    project is on going

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ import CollaborateWork from "@/components/view/CollaborateWork";
 import ContactUs from "@/components/view/ContactUs";
 import HappyCustomer from "@/components/view/HappyCustomer";
 import LiveChat from "@/components/view/LiveChat";
-import OurProject from "@/components/view/OurProject";
+import OurProject from "@/components/view/OurProject2";
 import OurService from "@/components/view/OurService";
 import OurTeam from "@/components/view/OurTeam";
 import Testimonial from "@/components/view/Testimonial";
---
+++ src/app/our-project/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+
+import CollaborateWork from "@/components/view/CollaborateWork";
+import Project from "@/components/view/Project/Project";
+import Image from "next/image";
+
+const OurProject = () => {
+  return (
+    <div className="container mx-auto">
+      {/* Banner Section */}
+      <div className="relative w-full h-[300px] sm:h-[350px] lg:h-[400px] flex items-center justify-center bg-black">
+        <Image
+          src="https://i.postimg.cc/bwTh0nJQ/look-studio.jpg"
+          alt="About Us Banner"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-50"
+        />
+        <div className="absolute text-center text-white px-4">
+          <h1 className="text-4xl lg:text-6xl font-extrabold sm:text-base">
+            Our Blogs
+          </h1>
+          <p className="mt-2 text-sm sm:text-base">Home / Pages / Our Blogs</p>
+        </div>
+      </div>
+
+      {/* Content Section */}
+      <div className="mt-6 sm:mt-12">
+        <Project />
+      </div>
+
+      <div className="mt-6 sm:mt-12">
+        <CollaborateWork />
+      </div>
+    </div>
+  );
+};
+
+export default OurProject;
