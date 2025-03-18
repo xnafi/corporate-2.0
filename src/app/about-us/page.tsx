4a2a1cd8a0addca3f054f654c93591947d6704d4
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-18 11:05:06 +0600

    restyle & restructered bannersaction in pages

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -8,35 +8,23 @@ import LiveChat from "@/components/view/LiveChat";
 import OurService from "@/components/view/OurService";
 import OurTeam from "@/components/view/OurTeam";
 import WhyChooseUs from "@/components/view/WhyChooseUs";
-import Image from "next/image";
+import BannerSection from "@/components/re-ui/BannerSection";
 
 const AboutUs = () => {
   return (
     <div className="container mx-auto">
       {/* Banner Section */}
-      <div className="relative w-full h-[400px] flex items-center justify-center bg-black">
-        <Image
-          src="https://i.postimg.cc/bwTh0nJQ/look-studio.jpg"
-          alt="About Us Banner"
-          layout="fill"
-          objectFit="cover"
-          className="opacity-50"
-        />
-        <div className="absolute text-center text-white">
-          <h1 className="bnr-header-text">About Us</h1>
-          <p className="banner-para-text">Home / Pages / About Us</p>
-        </div>
-      </div>
-          
-        {/* Content Section */}
-        <AboutUsSpecialists />
-        <OurService />
-        <WhyChooseUs />
-        <OurTeam />
-        <Feedback2 />
-        <ContactUs />
-        <LiveChat />
-        <CollaborateWork />
+      <BannerSection />
+
+      {/* Content Section */}
+      <AboutUsSpecialists />
+      <OurService />
+      <WhyChooseUs />
+      <OurTeam />
+      <Feedback2 />
+      <ContactUs />
+      <LiveChat />
+      <CollaborateWork />
     </div>
   );
 };
--- src/app/our-services/page.tsx
+++ src/app/our-services/page.tsx
@@ -2,27 +2,13 @@
 
 import CollaborateWork from "@/components/view/CollaborateWork";
 import OurService2 from "@/components/view/OurService2";
-import Image from "next/image";
+import BannerSection from "@/components/re-ui/BannerSection";
 
 const OurServices = () => {
   return (
     <div className="container mx-auto">
       {/* Banner Section */}
-      <div className="relative w-full h-[300px] sm:h-[350px] lg:h-[400px] flex items-center justify-center bg-black">
-        <Image
-          src="https://i.postimg.cc/bwTh0nJQ/look-studio.jpg"
-          alt="About Us Banner"
-          layout="fill"
-          objectFit="cover"
-          className="opacity-50"
-        />
-        <div className="absolute text-center text-white px-4">
-          <h1 className="bnr-header-text">
-            Our Services
-          </h1>
-          <p className="banner-para-text">Home / Pages / Our Services</p>
-        </div>
-      </div>
+      <BannerSection />
 
       {/* Content Section */}
       <div className="mt-6 sm:mt-12">
