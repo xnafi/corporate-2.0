63abba9d6eebf7b1c70e7b353ef5bc08592869eb
Forhad Khan <fk.nafi11@gmail.com>
2025-04-12 00:11:11 +0600

    Live chat done

--- src/components/view/LiveChat.tsx
+++ src/components/view/LiveChat.tsx
@@ -5,8 +5,8 @@ const LiveChat = () => {
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-12">
         {/* Help Section */}
-        <div className="flex items-center text-center sm:text-start gap-4">
-          <div className="w-12 h-12 flex items-center justify-center bg-purple-500 text-white rounded-full">
+        <div className="flex flex-col md:flex-row items-center text-center sm:text-start gap-4">
+          <div className="w-12 h-12 flex items-center justify-center bg-[#1A73E8] text-white rounded-full">
             <FaHeart size={20} />
           </div>
           <div>
@@ -20,11 +20,11 @@ const LiveChat = () => {
         </div>
 
         {/* Divider */}
-        <div className="hidden md:block w-px h-10 bg-gray-400"></div>
+        <div className="hidden md:block w-px h-10 bg-[#1A73E8]"></div>
 
         {/* Hotline Section */}
-        <div className="flex items-center text-center sm:text-start gap-4">
-          <div className="w-12 h-12 flex items-center justify-center bg-orange-500 text-white rounded-full">
+        <div className="flex flex-col md:flex-row items-center text-center sm:text-start gap-4">
+          <div className="w-12 h-12 flex items-center justify-center bg-[#1A73E8] text-white rounded-full">
             <FaPhoneAlt size={20} />
           </div>
           <div>
@@ -38,14 +38,14 @@ const LiveChat = () => {
         </div>
 
         {/* Divider */}
-        <div className="hidden md:block w-px h-10 bg-gray-400"></div>
+        <div className="hidden md:block w-px h-10 bg-[#1A73E8]"></div>
 
         {/* Live Chat Section */}
-        <div className="flex items-center text-center sm:text-start gap-8 lg:gap-4">
-          <div className="w-12 h-12  flex items-center justify-center bg-green-500 text-white rounded-full">
+        <div className="flex flex-col md:flex-row items-center gap-4 text-center md:text-left">
+          <div className="w-12 h-12 flex items-center justify-center bg-[#1A73E8] text-white rounded-full">
             <FaComments size={20} />
           </div>
-          <div className="pr-6">
+          <div>
             <p className="text-gray-700 font-medium text-sm sm:text-base md:text-lg">
               Live chat
             </p>
