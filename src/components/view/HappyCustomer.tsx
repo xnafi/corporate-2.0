fbe34ebc2028d5fe34094be5f3129c04c0a027aa
Forhad Khan <fk.nafi11@gmail.com>
2025-04-12 16:15:16 +0600

    countUp created up and re edit happy customer section

--- src/components/view/HappyCustomer.tsx
+++ src/components/view/HappyCustomer.tsx
@@ -1,22 +1,13 @@
-"use client";
-
-import React from "react";
-import CountUp from "react-countup";
-import { useInView } from "react-intersection-observer";
+import CountUp from "@/utils/countUp";
 
 const HappyCustomer: React.FC = () => {
-  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
-
   return (
-    <div ref={ref} className="container mx-auto px-4 lg:py-2">
+    <div className="container mx-auto px-4 lg:py-2">
       <div className="flex flex-col md:flex-row justify-between items-center text-center gap-6 md:gap-16 lg:gap-20">
         {/* Happy Customers */}
         <div className="w-full sm:w-auto md:text-start">
           <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
-            {inView && (
-              <CountUp end={1200000} separator="," duration={3} delay={0.5} />
-            )}
-            +
+            <CountUp from={0} to={120000} duration={2} span="+" />
           </h2>
           <p className="text-gray-500 text-sm sm:text-base mt-2">
             Happy Customers
@@ -26,9 +17,7 @@ const HappyCustomer: React.FC = () => {
         {/* Expert Members */}
         <div className="w-full sm:w-auto md:text-start">
           <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
-            {inView && (
-              <CountUp end={1200} separator="," duration={3} delay={0.5} />
-            )}
+            <CountUp from={0} to={1200} duration={2} span="+" />
           </h2>
           <p className="text-gray-500 text-sm sm:text-base mt-2">
             Expert Members
@@ -38,7 +27,7 @@ const HappyCustomer: React.FC = () => {
         {/* Years Experience */}
         <div className="w-full sm:w-auto md:text-start">
           <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
-            {inView && <CountUp end={14} duration={3} delay={0.5} />}+
+            <CountUp from={0} to={14} duration={2} span="+" />
           </h2>
           <p className="text-gray-500 text-sm sm:text-base mt-2">
             Years Experience
